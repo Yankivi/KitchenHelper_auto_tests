81064b822771c4bdb3d8f8d2bf3edf65ba3266e5
Vika <vika.test.y@gmail.com>
2024-08-06 02:11:44 +0300

    git commit

--- conftest.py
+++ conftest.py
@@ -11,13 +11,13 @@ import globals
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
-    driver.get("https://forms-s9jo.onrender.com/")
+    driver.get(globals.main_page)
     yield driver
     driver.quit()
 
 @pytest.fixture
 def admin_enter(driver):
-    driver.get("https://forms-s9jo.onrender.com/")
+    driver.get(globals.main_page)
 
     driver.find_element('xpath', '/html/body/main/form[1]/button').click()
     driver.find_element(By.XPATH, '//*[@id="password"]').send_keys(globals.admin_pass)
